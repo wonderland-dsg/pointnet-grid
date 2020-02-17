8a4e453ff16791cc59af23b28d1b3793727dec66
dangsuogui <dangsuogui@foxmail.com>
2020-02-17 15:14:28 +0800

    add grid code

---
+++ models/pointnet_grid_basic.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import numpy as np
+import math
+import sys
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, '../utils'))
+import tf_util
+
+def placeholder_inputs(batch_size, num_point):
+    pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
+    labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
+    return pointclouds_pl, labels_pl
+
+
+def get_model(point_cloud, is_training, bn_decay=None):
+    """ Classification PointNet, input is BxNx3, output Bx40 """
+    batch_size = point_cloud.get_shape()[0].value
+    num_point = point_cloud.get_shape()[1].value
+    end_points = {}
+    point_cloud = tf.reshape(point_cloud, [-1, 3]) 
+    net = tf_util.fully_connected(point_cloud, 100*20, bn=False, is_training=is_training,
+                                  scope='grid_w', bn_decay=bn_decay)
+    
+    net = tf.exp(tf.complex(.0, net))
+
+    c = tf_util._variable_with_weight_decay('c',
+                                shape=[100, 20],
+                                use_xavier=True,
+                                stddev=0.01,
+                                wd=0.001)
+    net = tf.reshape(net, [-1, 100, 20]) * tf.complex(c, .0)  
+    net = tf.reduce_sum(net, axis=2)
+    net = tf.concat([tf.real(net), tf.imag(net)], axis=1)
+    net = tf.reduce_sum( tf.reshape(net, [-1, num_point, 200]), axis=1)
+
+    # MLP on global point cloud vector
+    net = tf.reshape(net, [batch_size, -1])
+    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
+                                  scope='fc1', bn_decay=bn_decay)
+    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
+                                  scope='fc2', bn_decay=bn_decay)
+    net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training,
+                          scope='dp1')
+    net = tf_util.fully_connected(net, 40, activation_fn=None, scope='fc3')
+
+    return net, end_points
+
+
+def get_loss(pred, label, end_points):
+    """ pred: B*NUM_CLASSES,
+        label: B, """
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=label)
+    classify_loss = tf.reduce_mean(loss)
+    tf.summary.scalar('classify loss', classify_loss)
+    return classify_loss
+
+
+if __name__=='__main__':
+    with tf.Graph().as_default():
+        inputs = tf.zeros((32,1024,3))
+        outputs = get_model(inputs, tf.constant(True))
+        print(outputs)
